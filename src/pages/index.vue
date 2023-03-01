f408411c83558d283218f4d13be8d1f65a33de46
kevin <kevin.huang@hcfim.com>
2023-03-01 17:08:22 +0800

    ci:集成pinia

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,16 +5,18 @@
     <!-- <NuxtLink to="/demo">333</NuxtLink> -->
     <NuxtLink to="/demo/d-333">4444</NuxtLink>
 
-<a href="javascript:;" @click="setLang('zh')">简体中文</a>
-<a href="javascript:;" @click="setLang('en')">English</a>
+    <a href="javascript:;" @click="setLang('zh')">简体中文</a>
+    <a href="javascript:;" @click="setLang('en')">English</a>
   </div>
 </template>
 <script setup>
+import { useUserStore } from '@/store'
 import { useRouter } from 'vue-router'
-    import { useI18n } from "vue-i18n";
-    const { t,locale } = useI18n();
-
+import { useI18n } from "vue-i18n";
+const { t, locale } = useI18n();
+const userInfo = useUserStore().userInfo
+console.log(userInfo,'---userInfo');
 const setLang = (lang) => {
-  locale.value =lang;
+  locale.value = lang;
 }
 </script>
