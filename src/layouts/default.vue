8b3114ea997057361e1deda4551bc85baafb1180
kevin <kevin.huang@hcfim.com>
2023-03-07 16:52:13 +0800

    modify:更改layouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>我是头部</div>
+    <main class="py-2 px-10 text-center">
+      <slot />
+      <div class="mt-5 mx-auto text-center opacity-25 text-sm">
+        我是地步
+      </div>
+    </main>
+  </div>
+</template>
